bfde2dc342eb4b3647f064f2cee116c4e65def18
NielsIH <github@acidabse.nl>
2025-09-30 17:01:35 +0200

    Split modals.css into multiple files. Added styling for import decision modal

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* global caches self */
 
-const CACHE_NAME = 'image-mapper-v2025-09-30-59' // Updated version
+const CACHE_NAME = 'image-mapper-v2025-09-30-63' // Updated version
 const STATIC_ASSETS = [
   './',
   './index.html',
