d22e24936911f5b8eb04a84e470a0988d4a05229
Niels Ingen Housz <niels.ingenhousz@mumc.nl>
2025-11-12 12:54:51 +0100

    chore: Update service-worker.js (formatting/minor changes)

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* global caches self */
 
-const CACHE_NAME = 'image-mapper-v2025-11-12-06' // Updated version
+const CACHE_NAME = 'image-mapper-v2025-11-12-07' // Updated version
 const STATIC_ASSETS = [
   './',
   './index.html',
